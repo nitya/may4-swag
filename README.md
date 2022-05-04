6bc38982b83e69e10446a5bb56cceed05a95cf39
Nitya Narasimhan <nitya@users.noreply.github.com>
2022-05-04 12:07:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# :sunglasses: Azure SWAG demo
+
+This is a simple barebone demo for [Azure SWAG](https://docs.microsoft.com/azure/static-web-apps?WT.mc_id=javascript-0000-yolasors) (**S**tatic **W**eb **A**pps on **G**itHub).
+You can use it as a template for your own apps.
+
+It includes a website using HTML + JavaScript and an example Node.js API.
+See it live [here](https://aka.ms/swag-demo).
+
+Click on **Use this template** on GitHub, then click on the button below to deploy your own (for free): 
+
+[![Deploy to Azure button](https://aka.ms/deploytoazurebutton)](https://portal.azure.com/?feature.customportal=false&WT.mc_id=javascript-0000-yolasors#create/Microsoft.StaticApp)
+
+## What's Azure *SWAG*?
+
+TL;DR:
+
+> It's an all-inclusive hosting service for web apps with serverless APIs built using JavaScript, based on continuous deployment from a GitHub repository.
+
+For the longer version with the deployment instructions, [see this full length article](https://dev.to/sinedied/the-easy-way-to-serverless-web-apps-and-apis-with-azure-swag-2heb).
+
+You can also take a look at [the official docs](https://docs.microsoft.com/azure/static-web-apps?WT.mc_id=javascript-0000-yolasors).
---
+++ api/quote/index.js
@@ -0,0 +1,40 @@
+const https = require('https');
+
+const unplashApi = 'https://source.unsplash.com/1600x900?dream';
+const quotes = [
+  'Wherever you go, no matter what the weather, always bring your own sunshine.',
+  'You\’re awesome.',
+  'Happiness is the only thing that multiplies when you share it.',
+  'It always seems impossible until it is done.',
+  'Let your unique positive energy inspire confidence in others.',
+  'The best is yet to come.',
+  'You\'re capable of more than you can even dream.',
+  'You deserve the best.',
+  'Keep going, you\'re doing well.',
+  'Stay positive; stay hopeful'
+];
+
+async function getImage() {
+  return new Promise((resolve, reject) => {
+    https.get(unplashApi, (response) => {
+      // API returns a HTTP 302 code, we only want the final image URL
+      resolve(response.headers.location);
+    }).on('error', (error) => {
+      reject(error.message);
+    });
+  });
+}
+
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
+
+  const image = await getImage();
+  const text = quotes[Math.floor(Math.random() * quotes.length)];
+
+  context.res = {
+    body: {
+      image,
+      text
+    }
+  };
+};
